d66aeee8e5138a223e0ee562af915ed9b7dc9108
shubham-s-del <shuhbam.s.del@gmail.com>
2015-06-06 01:57:35 +0530

    Other basic setup

--- src/main/java/Store/SellerDao.java
+++ src/main/java/Store/SellerDao.java
@@ -1,7 +1,23 @@
 package Store;
 
+import Store.support.SellerInfo;
+
+import java.util.List;
+
 /**
  * Created by shubham.srivastava on 05/06/15.
  */
-public class SellerDao {
+public interface SellerDao {
+
+    void insert(SellerInfo sellerInfo);
+
+    void deleteAllSellers();
+
+    List<String> findNearest(double latitude, double longitude, int number);
+
+    List<String> findAll(double latitude, double longitude);
+
+    SellerInfo findByEmailId(String emailId);
+
+
 }
